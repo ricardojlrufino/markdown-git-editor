23dbdb5e1f9c2dc757db52e51136614c42cb5d37
Ricardo JL Rufino <ricardo@edu3.com.br>
2022-11-20 23:16:57 -0300

    improove sidebar

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -8,30 +8,32 @@ export function SideBar() {
 
   useEffect(() => {
     
-    const list = document.querySelectorAll('.list');
+   const list = document.querySelectorAll('.ac-list');
 
-    function accordion(e){
-        e.stopPropagation(); 
-        e.preventDefault();
+   function accordion(e) {
+      e.stopPropagation();
+      e.preventDefault();
 
-        if(this.classList.contains('active')){
-            this.classList.remove('active');
-        }
-        else if(this.parentElement.parentElement.classList.contains('active')){
-            this.classList.add('active');
-        }
-        else{
-            for(let i = 0; i < list.length; i++){
-              list[i].classList.remove('active');
-            }
-                this.classList.add('active');
-            }
-    }
+      console.log(e.target.tagName, this.tagName);
 
-    // Add handler
-    for(let i = 0; i < list.length; i++ ){
-        list[i].addEventListener('click', accordion);
-    }
+      if (this.classList.contains('active')) {
+         this.classList.remove('active');
+      } else if (this.parentElement.parentElement.classList.contains('active')) {
+         this.classList.add('active');
+      }else {
+
+         // for (let i = 0; i < list.length; i++) {
+         //    list[i].classList.remove('active');
+         // }
+
+         this.classList.add('active');
+      }
+   }
+
+   // Add handler
+   for(let i = 0; i < list.length; i++ ){
+      list[i].addEventListener('click', accordion);
+   }
 
 
   }, [])
@@ -41,36 +43,36 @@ export function SideBar() {
     <>
    
    <ul class="menu">
-   <li class="list"><a href="#">List 1 </a> 
+   <li class="ac-list"><a href="#">List 1 </a> 
       <ul class="items">
-         <li><a href="#"> Item 1-1</a></li>
-         <li><a href="#"> Item 1-2</a></li>
+         <li><a href="/page/docs/Popcorn.md"> Item 1-1</a></li>
+         <li><a href="/page/docs/Readme.md"> Item 1-2</a></li>
          <li><a href="#"> Item 1-3</a></li>
       </ul>
    </li>
-   <li class="list"><a href="#">List 2</a> 
+   <li class="ac-list"><a href="#">List 2</a> 
       <ul class="items">
          <li> <a href="#" > Item 2-1 </a></li>
          <li> <a href="#" > Item 2-2 </a></li>
          <li> <a href="#" > Item 2-3 </a></li>
       </ul>
    </li>
-   <li class="list"><a href="#">List 3</a> 
+   <li class="ac-list"><a href="#">List 3</a> 
       <ul class="items">
          <li> <a href="#" > Item 3-1 </a></li>
          <li> <a href="#" > Item 3-2 </a></li>
          <li> <a href="#" > Item 3-2 </a></li>
       </ul>
    </li>
-   <li class="list"><a href="#">List 4</a> 
+   <li class="ac-list"><a href="#">List 4 (l:0)</a> 
       <ul class="items">
          <li> <a href="#" > Item 4-1 </a></li>
          <li> <a href="#" > Item 4-2 </a></li>
-         <li class="list"><a href="#">List 4-1</a> 
+         <li class="ac-list"><a href="#">List 4-1 (l:1)</a> 
             <ul  class="items">
                <li> <a href="#" > Item 4-1-1</a></li>
                <li> <a href="#" > Item 4-1-2</a></li>
-               <li class="list"><a href="#">List 4-2</a> 
+               <li class="ac-list"><a href="#">List 4-1-2 (l:2)</a> 
                   <ul  class="items">
                      <li> <a href="#" > Item 4-2-1</a></li>
                      <li> <a href="#" > Item 4-2-2</a></li>
